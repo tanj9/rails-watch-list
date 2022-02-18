933a97a5d61f9641f1c943892831a28b8d4be26f
Jerome Tan <jerome.tan@tuta.io>
2022-02-18 11:55:02 +0100

    Watch List Day 2 setup heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, except: %i[edit update destroy] do
+  root to: 'lists#index'
+  resources :lists, only: %i[new show create] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: [:destroy]
